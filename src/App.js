a916b526aa2d4f30f80cdcefdc64090a9dacf989
wsbousman <85909621+wsbousman@users.noreply.github.com>
2021-11-19 15:15:50 -0600

    Up1
    Apps.js framed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import About from './components/About';
+import ContactForm from './components/Contact';
+import Nav from './components/Nav';
+import Project from './components/Project';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
+  const [categories] = useState([
+    { name: 'About', description: '', },
+    { name: 'Portfolio', description: 'Endlessly pursuing clean code' },
+    { name: 'Contact', description: 'Slide into the DMs' },
+    { name: 'Resume', description: 'Client-facing experience' },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+      <Nav
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
+      ></Nav>
+      </Header>
+      <main>
+        {!contactSelected ? (
+        <>
+        {/* <Gallery currentCategory={currentCategory}></Gallery> */}
+        <About></About>
+        </>
+        ) : (<ContactForm></ContactForm>)}
+      <Footer></Footer>
+      </main>
     </div>
   );
 }
