2a1537450d3344734ec6780ec99215b1bef8984e
wsbousman <85909621+wsbousman@users.noreply.github.com>
2021-11-23 03:40:14 -0600

    Up11
    Navbar links, CSS refactoring

--- src/components/Nav/Index.js
+++ src/components/Nav/Index.js
@@ -5,10 +5,11 @@ function Nav() {
 
   return (
     <header>
-        <nav>
-
+        <nav class="nav">
+            <Link to="/">About</Link>
             <Link to="/Project">Portfolio</Link>
-       
+            <Link to="/Contact">Contact</Link>
+            {/*<Link to="/Resume">Resume</Link>*/}
         </nav>
     </header>
   );
