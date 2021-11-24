ff5d9e6cc598147a1740b7489430c4e357fa6e5b
wsbousman <85909621+wsbousman@users.noreply.github.com>
2021-11-23 18:08:39 -0600

    MVP
    MVP

--- src/components/Project/Index.js
+++ src/components/Project/Index.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import genny from "../../Assets/genny.png";
+import thotimus from "../../Assets/thotimus.png";
+import timerex from "../../Assets/timerex.png";
 
 function project() {
 
   return (
-    <section className="my-5" >
-      <h1 id="about">project</h1>
-      <div className="my-2">
-        <p>
-            test
-        </p>
+    <section>
+      <p className="port2">portfolio</p>
+      <div className="wrap">
+        <a href="https://github.com/wsbousman/Genny"><img src={genny} alt="random password generator" /></a>
       </div>
+        <p className="port">Genny is a JavaScript random password generator with a variety of optional character sets.</p>
+      <div className="wrap">
+        <a href="https://github.com/wsbousman/Thotimus-Prime"><img src={thotimus} alt="a social media back end built using MongoDB" /></a>
+      </div>
+        <p className="port">Thotimus Prime is a MongoDB database designed to support a thought-sharing social media app.</p>
+      <div className="wrap">
+        <a href="https://github.com/wsbousman/GetTimer"><img src={timerex} alt="a timer built without setInterval or setTimeout methods" /></a>
+      </div>
+        <p className="port">getTimer is a Javascript timer built without using setInterval or setTimeout methods, just for fun.</p>
     </section>
   )
 }
