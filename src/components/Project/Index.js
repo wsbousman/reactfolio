453278bd4c380b8d4bf98eb0bb518748b2e2195a
wsbousman <85909621+wsbousman@users.noreply.github.com>
2021-12-23 20:11:30 -0600

    HTML/CSS port
    HTML/CSS portfolio link added in project component

--- src/components/Project/Index.js
+++ src/components/Project/Index.js
@@ -3,11 +3,17 @@ import genny from "../../Assets/genny.png";
 import thotimus from "../../Assets/thotimus.png";
 import timerex from "../../Assets/timerex.png";
 import reactfolio from "../../Assets/reactfolio.png";
+import htmlport from "../../Assets/htmlport.png";
+
 
 function project() {
 
   return (
     <section>
+      <div className="wrap">
+        <a href="https://github.com/wsbousman/reactfolio"><img src={reactfolio} alt="a React portfolio" /></a>
+      </div>
+        <p className="port">I built the site you're currently viewing using React.</p>
       <div className="wrap">
         <a href="https://github.com/wsbousman/Genny"><img src={genny} alt="random password generator" /></a>
       </div>
@@ -22,9 +28,9 @@ function project() {
       </div>
         <p className="port">Thotimus Prime is a MongoDB database designed to support a thought-sharing social media app.</p>
       <div className="wrap">
-        <a href="https://github.com/wsbousman/reactfolio"><img src={reactfolio} alt="a React portfolio" /></a>
+        <a href="https://wsbousman.github.io/Port/"><img src={htmlport} alt="an HTML version of this portfolio" /></a>
       </div>
-        <p className="port">And I built the site you're currently viewing using React.</p>
+        <p className="port">I also have an HTML/CSS-only, mobile-friendly version of this portfolio.</p>
       <p className="port2">A much greater variety of my work, including group projects I contributed to, can be found at my GitHub.</p>
     </section>
   )
