d5ac16a783d9ffbf9d4a473d97650b8fe95dd1b4
wsbousman <85909621+wsbousman@users.noreply.github.com>
2021-12-31 00:30:16 -0600

    PNGs/Spin test
    Transparent BGs on all portfolio link images. BG spin CSS test.

--- src/components/Project/Index.js
+++ src/components/Project/Index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import genny from "../../Assets/genny.png";
-import thotimus from "../../Assets/thotimus.png";
-import timerex from "../../Assets/timerex.png";
-import reactfolio from "../../Assets/reactfolio.png";
-import htmlport from "../../Assets/htmlport.png";
+import genny from "../../Assets/gennyPNG.png";
+import thotimus from "../../Assets/thotimusPNG.png";
+import timerex from "../../Assets/timerexPNG.png";
+import reactfolio from "../../Assets/reactfolioPNG.png";
+import htmlport from "../../Assets/htmlportPNG.png";
 
 
 function project() {
@@ -17,12 +17,12 @@ function project() {
       <div className="wrap">
         <a href="https://github.com/wsbousman/Genny"><img src={genny} alt="random password generator" /></a>
       </div>
-        <p className="port">Genny is a JavaScript random password generator that iterates over a variety of optional arrays of Unicode character codes.</p>
+        <p className="port">Genny is a JavaScript password generator that randomly iterates over a variety of optional arrays of Unicode character codes.</p>
     
       <div className="wrap">
         <a href="https://github.com/wsbousman/GetTimer"><img src={timerex} alt="a timer built without setInterval or setTimeout methods" /></a>
       </div>
-        <p className="port">getTimer is a Javascript timer built without using setInterval or setTimeout methods, just for fun.</p>
+        <p className="port">getTimer is a JavaScript timer built without using setInterval or setTimeout methods, just for fun.</p>
       <div className="wrap">
         <a href="https://github.com/wsbousman/Thotimus-Prime"><img src={thotimus} alt="a social media back end built using MongoDB" /></a>
       </div>
